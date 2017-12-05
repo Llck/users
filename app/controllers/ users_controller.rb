cb1ab7ca68a4a114507e326bbb275b65cb05d3ca
Llck <mandarinu2013njamz@gmail.com>
2017-12-05 10:49:41 +0200

    ups

--- app/controllers/ users_controller.rb
+++ app/controllers/ users_controller.rb
@@ -1,34 +1,39 @@
 class UsersController < ApplicationController
 
 def index
-  @client = User.all
+  @users = User.all
 end
 
 def new
-  @client = User.new
+  @user = User.new
 end
 
 def show
-  @client = User.find(params[:id])
+  @user = User.find(params[:id])
 end
+
 def create
    user = User.new(user_params)
    user.save
    redirect_to user
 end
-def edit
-  @client = User.find(params[:id])
-end
-def update
-  @client = User.find(params[:id])
 
-  @client.update(user_params)
+def edit
+  @user = User.find(params[:id])
 end
+
+def update
+  @user = User.find(params[:id])
+
+  @user.update(user_params)
+end
+
 def destroy
-  @client = User.find(params[:id])
-  @client.destroy
+  @user = User.find(params[:id])
+  @user.destroy
   redirect_to users_path
 end
+
 def user_params
   params.require(:post).permit(:name, :email)
 end
