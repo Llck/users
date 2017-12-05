076c3257aa9a8688693cb6cbb0a72f2bb595ef5a
Llck <mandarinu2013njamz@gmail.com>
2017-12-05 10:33:07 +0200

    errors

--- app/controllers/ users_controller.rb
+++ app/controllers/ users_controller.rb
@@ -5,15 +5,32 @@ def index
 end
 
 def new
-
+  @client = User.new
 end
 
 def show
-
+  @client = User.find(params[:id])
 end
 def create
-
+   user = User.new(user_params)
+   user.save
+   redirect_to user
 end
+def edit
+  @client = User.find(params[:id])
+end
+def update
+  @client = User.find(params[:id])
 
+  @client.update(user_params)
+end
+def destroy
+  @client = User.find(params[:id])
+  @client.destroy
+  redirect_to users_path
+end
+def user_params
+  params.require(:post).permit(:name, :email)
+end
 
 end
